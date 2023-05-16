8b0c8a9d696f447a60b8c268a4b68a771bc12349
Jacob Sohl <jacobsohl@gmail.com>
2023-05-15 22:58:00 -0700

    Work on dall-e components

--- cmd/dalle.go
+++ cmd/dalle.go
@@ -5,13 +5,14 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
+	"image"
 	"image/png"
 	"os"
 
 	openai "github.com/sashabaranov/go-openai"
 )
 
-func imageRequest(prompt string, outputFile string) error {
+func imageRequest(prompt string, size string, number int, outputFile string) error {
 	ic := openai.NewClient(getAPIToken())
 	ctx := context.Background()
 	// Example image as base64
@@ -41,19 +42,58 @@ func imageRequest(prompt string, outputFile string) error {
 		return err
 	}
 
-	file, err := os.Create(outputFile)
-	if err != nil {
-		fmt.Printf("File creation error: %v\n", err)
-		return err
-	}
-	defer file.Close()
+	createPNG(prompt, imgData, outputFile)
+	// file, err := os.Create(outputFile)
+	// if err != nil {
+	// 	fmt.Printf("File creation error: %v\n", err)
+	// 	return err
+	// }
+	// defer file.Close()
 
-	if err := png.Encode(file, imgData); err != nil {
-		fmt.Printf("PNG encode error: %v\n", err)
-		return err
-	}
+	// if err := png.Encode(file, imgData); err != nil {
+	// 	fmt.Printf("PNG encode error: %v\n", err)
+	// 	return err
+	// }
 
 	fmt.Printf("The image was saved as %s", outputFile)
 
 	return nil
+}
+
+func createPNG(prompt string, image image.Image, outputfile string) {
+	var err error
+	// Create the metadata
+	metadata := make(map[string]string)
+	metadata["prompt"] = prompt
+
+	// Attach the metadata to the PNG image
+	buf := new(bytes.Buffer)
+	err = png.Encode(buf, image)
+	if err != nil {
+		panic(err)
+	}
+	pngBytes := buf.Bytes()
+	pngBytes = append(pngBytes[0:33], encodeMetadata(metadata)...)
+	err = saveFile(pngBytes, outputfile)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("The image was saved as %s", outputfile)
+}
+
+func encodeMetadata(metadata map[string]string) []byte {
+	var buf bytes.Buffer
+	//TODO
+	return buf.Bytes()
+}
+
+func saveFile(data []byte, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Write(data)
+	return err
 }
--- cmd/imageCmd.go
+++ cmd/imageCmd.go
@@ -5,6 +5,8 @@ import "github.com/spf13/cobra"
 var (
 	output string
 	prompt string
+	size   string
+	number int
 )
 
 var imageCmd = &cobra.Command{
@@ -12,7 +14,7 @@ var imageCmd = &cobra.Command{
 	Short: "Generates an image based on a prompt",
 	Long:  `This command generates an image based on the provided prompt and saves it to the specified output file.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		imageRequest(prompt, output)
+		imageRequest(prompt, size, number, output)
 	},
 }
 
@@ -20,6 +22,8 @@ func init() {
 	conf.initConfigs()
 	imageCmd.Flags().StringVarP(&output, "output", "o", "", "Output file for the generated image (required)")
 	imageCmd.Flags().StringVarP(&prompt, "prompt", "p", "", "Prompt for generating the image (required)")
+	imageCmd.Flags().StringVarP(&size, "size", "s", "256", "size of image to generate defaults to options: 256 (default), 512, 1024")
+	imageCmd.Flags().IntVarP(&number, "num", "n", 1, "number of images to generate. Default: 1")
 	imageCmd.MarkFlagRequired("output")
 	imageCmd.MarkFlagRequired("prompt")
 	rootCmd.AddCommand(imageCmd)
